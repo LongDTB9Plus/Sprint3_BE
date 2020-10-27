45c3a00811821f972e3bd810959826917ac49714
phuong <daut.phuong@gmail.com>
2020-10-27 11:10:05 +0700

    update login and jwt

--- src/main/java/com/parking/models/security/config/WebSecurityConfig.java
+++ src/main/java/com/parking/models/security/config/WebSecurityConfig.java
@@ -1,4 +1,6 @@
 package com.parking.models.security.config;
 
-public class WebSecurityConfig {
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 }
