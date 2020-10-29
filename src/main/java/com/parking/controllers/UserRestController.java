b9b2ae3948529b0d4c2928fa3967b0b07bbe6075
phuong <daut.phuong@gmail.com>
2020-10-29 11:45:00 +0700

    create controller login and config websecurity

---
+++ src/main/java/com/parking/controllers/UserRestController.java
@@ -0,0 +1,62 @@
+package com.parking.controllers;
+
+import com.parking.models.security.login_msg.request.Login;
+import com.parking.models.security.login_msg.response.JwtResponse;
+import com.parking.models.security.utils.UserPrincipal;
+import com.parking.services.security.JwtProvider;
+import com.parking.services.security.user.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.validation.Valid;
+
+public class UserRestController {
+    @Autowired
+    UserService userService;
+
+    @Autowired
+    JwtProvider jwtProvider;
+    @Autowired
+    AuthenticationManager authManager;
+
+    private PasswordEncoder encoder;
+    @Autowired
+    public void setEncoder(PasswordEncoder encoder) {
+        this.encoder = encoder;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> authenticateUser(@Valid @RequestBody Login loginRequest) throws AuthenticationException {
+
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        loginRequest.getUsername(),
+                        loginRequest.getPassword()
+                )
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        String token = jwtProvider.generatingJwt(authentication);
+        System.out.println("Token is generated: " + token);
+
+        UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+        System.out.println("UserDetails: " + userPrincipal.getUsername());
+
+        JwtResponse response = new JwtResponse(
+                token,
+                userPrincipal.getUsername(),
+                userPrincipal.getEmail(),
+                userPrincipal.getAuthorities()
+        );
+        System.out.println(response);
+        return ResponseEntity.ok(response);
+    }
+}
--- src/main/java/com/parking/models/security/config/WebSecurityConfig.java
+++ src/main/java/com/parking/models/security/config/WebSecurityConfig.java
@@ -1,7 +1,143 @@
 package com.parking.models.security.config;
 
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.parking.models.security.constant.ERoleName;
+import com.parking.models.security.user.User;
+import com.parking.models.security.utils.Role;
+import com.parking.repositories.RoleRepository;
+import com.parking.repositories.UserRepository;
+import com.parking.services.security.JwtEntryPoint;
+import com.parking.services.security.JwtFilter;
+import com.parking.services.security.UserDetailsServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Configuration
+@EnableWebMvc
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Value("${app.admin.username}")
+    private String adminUsername;
+
+    @Value("${app.admin.password}")
+    private String adminPassword;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtEntryPoint jwtEntryPoint;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public JwtFilter authenticationFilter() {
+        return new JwtFilter();
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    ApplicationRunner init(RoleRepository roleRepository, UserRepository userRepository) {
+        System.out.println("Created !");
+        return args -> {
+            if (roleRepository.findAll().isEmpty()) {
+
+                Role adminRole = new Role(ERoleName.ROLE_ADMIN);
+                adminRole.setId(1);
+
+                Role memberRole = new Role(ERoleName.ROLE_MEMBER);
+                memberRole.setId(2);
+
+                roleRepository.save(adminRole);
+                roleRepository.save(memberRole);
+
+                Set<Role> roles = new HashSet<>();
+                roles.add(roleRepository.findByRoleName(ERoleName.ROLE_ADMIN).orElseThrow(
+                        () -> new RuntimeException("Role doesn't exist")
+                ));
+
+                User admin = new User();
+
+                admin.setUsername(adminUsername);
+                admin.setPassword(passwordEncoder.encode(adminPassword));
+                admin.setFullName("ADMIN");
+                admin.setEmail("admin@gmail.com");
+                admin.setAddress("Da Nang");
+                admin.setPhone("0123456799");
+                admin.setRoles(roles);
+
+                userRepository.save(admin);
+
+                Set<Role> rolesForMember = new HashSet<>();
+                rolesForMember.add(roleRepository.findByRoleName(ERoleName.ROLE_MEMBER).orElseThrow(
+                        () -> new RuntimeException("Role doesn't exist")
+                ));
+
+
+                User member = new User();
+                member.setUsername("member");
+                member.setPassword(passwordEncoder.encode("123123"));
+                member.setFullName("MEMBER");
+                member.setEmail("member@gmail.com");
+                member.setAddress("Da Nang");
+                member.setPhone("0123456799");
+                member.setRoles(rolesForMember);
+                userRepository.save(member);
+            }
+        };
+    }
+
+    @Override
+    public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(userDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .cors().and().csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .and()
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http
+                .addFilterBefore(
+                        authenticationFilter(),
+                        UsernamePasswordAuthenticationFilter.class
+                );
+    }
 }
