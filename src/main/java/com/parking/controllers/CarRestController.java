dc7f44b35ec0b2e93645bdb0c5f95bd68fa3e688
BaolongDNH01 <61400179+BaolongDNH01@users.noreply.github.com>
2020-10-29 22:54:17 +0700

    update car controller

--- src/main/java/com/parking/controllers/CarRestController.java
+++ src/main/java/com/parking/controllers/CarRestController.java
@@ -1,14 +1,13 @@
 package com.parking.controllers;
 
 import com.parking.models.DAO.Car;
+import com.parking.models.DTO.CarDTO;
 import com.parking.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -22,6 +21,16 @@ public class CarRestController {
         if (car != null){
             return new ResponseEntity<>(car, HttpStatus.OK);
         }else return new ResponseEntity<>(HttpStatus.CONFLICT);
+    }
+
+    @PostMapping("add-car")
+    public ResponseEntity<Integer> addNewCar(@RequestBody CarDTO carDTO){
+        Car car = carService.convertToCar(carDTO);
+        Integer getID = carService.addNewCar(car);
+        if (getID != 0){
+            return new ResponseEntity<>(getID ,HttpStatus.CREATED);
+        }
+        else return new ResponseEntity<>(HttpStatus.CONFLICT);
     }
 
 }
