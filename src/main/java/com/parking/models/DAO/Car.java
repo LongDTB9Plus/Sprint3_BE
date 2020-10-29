2d3b73377d78a4d360df5e67d7486bdbd49b4b02
BaolongDNH01 <61400179+BaolongDNH01@users.noreply.github.com>
2020-10-29 10:48:40 +0700

    <Create> Entity Parking, Car

---
+++ src/main/java/com/parking/models/DAO/Car.java
@@ -0,0 +1,56 @@
+package com.parking.models.DAO;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer carId;
+    private String license;
+    private String color;
+    private String producer;
+    @OneToMany(mappedBy = "car")
+    private Set<Parking> parkings;
+
+    public Set<Parking> getParkings() {
+        return parkings;
+    }
+
+    public void setParkings(Set<Parking> parkings) {
+        this.parkings = parkings;
+    }
+
+    public Integer getCarId() {
+        return carId;
+    }
+
+    public void setCarId(Integer carId) {
+        this.carId = carId;
+    }
+
+    public String getLicense() {
+        return license;
+    }
+
+    public void setLicense(String license) {
+        this.license = license;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+}
